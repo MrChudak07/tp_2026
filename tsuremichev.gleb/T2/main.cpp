8b3b7f628dff0e79a8e3f3ba57334c332a5a46a5
mrchudak <gleb.ts2104@gmail.com>
2026-06-04 23:30:46 +0300

    Fix acceptance

--- tsuremichev.gleb/T2/main.cpp
+++ tsuremichev.gleb/T2/main.cpp
@@ -11,7 +11,6 @@ int main()
 {
   std::vector<DataStruct> data;
 
-  // Открываем файл для чтения
   std::ifstream infile("input.txt");
 
   if (!infile.is_open())
@@ -20,7 +19,6 @@ int main()
     return 1;
   }
 
-  // 1. Построчное чтение данных из файла
   while (!infile.eof())
   {
     std::copy(
@@ -37,27 +35,22 @@ int main()
 
   infile.close();
 
-  // 2. Сортировка по новым критериям (key1 — комплексное, key2 — double)
   std::sort(data.begin(), data.end(), [](const DataStruct &a, const DataStruct &b)
             {
-    const double EPS = 1e-9; // Погрешность сравнения double
+    const double EPS = 1e-9;
 
-    // 1. Сначала по возрастанию модуля комплексного числа key1
     double absA1 = std::abs(a.key1);
     double absB1 = std::abs(b.key1);
     if (std::abs(absA1 - absB1) > EPS) {
       return absA1 < absB1;
     }
 
-    // 2. Если модули key1 равны, сравниваем по возрастанию key2 (double)
     if (std::abs(a.key2 - b.key2) > EPS) {
       return a.key2 < b.key2;
     }
 
-    // 3. Если и тут равны, сравниваем по длине строки key3
     return a.key3.length() < b.key3.length(); });
 
-  // 3. Вывод отсортированных данных в консоль
   std::copy(
       data.begin(),
       data.end(),
